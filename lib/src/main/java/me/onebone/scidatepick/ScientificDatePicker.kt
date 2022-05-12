b9b7faa412b98c981e3506ba7d13da7d0a3a27af
onebone <jyc00410@gmail.com>
2022-05-13 00:38:22 +0900

    feat: initial commit

---
+++ lib/src/main/java/me/onebone/scidatepick/ScientificDatePicker.kt
@@ -0,0 +1,14 @@
+package me.onebone.scidatepick
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScientificDatePicker(
+	modifier: Modifier
+) {
+	Canvas(modifier = modifier) {
+		val size = size
+	}
+}
