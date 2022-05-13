2b38c430b2732b9d194c8dd369f46871ab61b410
onebone <jyc00410@gmail.com>
2022-05-13 13:06:27 +0900

    feat: get position from path

--- lib/src/main/java/me/onebone/scidatepick/ScientificDatePicker.kt
+++ lib/src/main/java/me/onebone/scidatepick/ScientificDatePicker.kt
@@ -1,17 +1,25 @@
 package me.onebone.scidatepick
 
+import android.graphics.PathMeasure
 import androidx.compose.foundation.Canvas
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.gestures.awaitDragOrCancellation
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathMeasure
+import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.consumePositionChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntSize
 
 @Composable
 fun ScientificDatePicker(
@@ -19,28 +27,65 @@ fun ScientificDatePicker(
 ) {
 	val padding = with(LocalDensity.current) { DefaultDatePickerPadding.toPx() }
 
-	Canvas(modifier = modifier) {
-		val size = size
+	var size by remember { mutableStateOf<IntSize?>(null) }
 
-		val path = Path().apply {
+	val path = remember(size) {
+		if (size == null) return@remember null
+
+		Path().apply {
 			addArc(
 				oval = Rect(
 					offset = Offset(padding, padding),
-					size = Size(size.width - padding * 2, size.height - padding * 2)
+					size = Size(size!!.width - padding * 2, size!!.height - padding * 2)
 				),
 				startAngleDegrees = 0f,
 				sweepAngleDegrees = 360f
 			)
 		}
+	}
 
-		drawPath(
-			path = path,
-			color = DefaultOrbitColor,
-			style = Stroke(width = 5f)
-		)
+	val measure = remember(path) { PathMeasure(path?.asAndroidPath(), false) }
+	val offsets = remember(measure) {
+		val length = measure.length
 
-		val measure = PathMeasure().apply {
-			setPath(path, false)
+		val fArr = FloatArray(2)
+
+		List(300) {
+			measure.getPosTan(length * it / 300f, fArr, null)
+			Offset(fArr[0], fArr[1])
+		}
+	}
+
+	Canvas(
+		modifier = modifier
+			.onGloballyPositioned {
+				size = it.size
+			}
+			.pointerInput(Unit) {
+				while (true) {
+					awaitPointerEventScope {
+						val pointer = awaitFirstDown()
+
+						val change = awaitDragOrCancellation(pointer.id) ?: return@awaitPointerEventScope
+
+						val position = change.position
+						val delta = change.positionChange()
+						change.consumePositionChange()
+					}
+				}
+			}
+	) {
+		if (path != null) {
+			drawPath(
+				path = path,
+				color = DefaultOrbitColor,
+				style = Stroke(width = 5f)
+			)
+		}
+
+		// TODO: remove it
+		offsets.forEach {
+			drawCircle(Color.Red, radius = 5f, center = it)
 		}
 	}
 }
