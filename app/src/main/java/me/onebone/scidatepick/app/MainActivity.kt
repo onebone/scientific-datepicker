b608a7e3528b191803bebe55eea38ea364e3d98d
onebone <jyc00410@gmail.com>
2022-10-31 04:38:59 +0900

    feat: implement inaccurate date change listener

--- app/src/main/java/me/onebone/scidatepick/app/MainActivity.kt
+++ app/src/main/java/me/onebone/scidatepick/app/MainActivity.kt
@@ -10,11 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import me.onebone.scidatepick.ScientificDatePicker
 import me.onebone.scidatepick.app.ui.theme.ScientificDatepickerTheme
+import java.time.LocalDate
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,12 +31,19 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colors.background
 				) {
+					var date by remember { mutableStateOf(LocalDate.now()) }
+
 					ScientificDatePicker(
 						modifier = Modifier
 							.background(Color.Black)
 							.padding(16.dp)
 							.fillMaxWidth()
-							.aspectRatio(2.5f)
+							.aspectRatio(2.5f),
+						date = date,
+						onDateChange = {
+							date = it
+							println(it)
+						}
 					)
 				}
 			}
--- lib/src/main/java/me/onebone/scidatepick/ScientificDatePicker.kt
+++ lib/src/main/java/me/onebone/scidatepick/ScientificDatePicker.kt
@@ -11,13 +11,16 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntSize
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
+import java.time.LocalDate
+import kotlin.math.*
+
+const val PI_F = PI.toFloat()
 
 @Composable
 fun ScientificDatePicker(
-	modifier: Modifier = Modifier
+	modifier: Modifier = Modifier,
+	date: LocalDate,
+	onDateChange: (LocalDate) -> Unit
 ) {
 	var earthOffset by remember {
 		mutableStateOf<Offset?>(null)
@@ -33,6 +36,7 @@ fun ScientificDatePicker(
 			.pointerInput(Unit) {
 				var accumulated: Offset = Offset.Zero
 				var startEarthOffset: Offset = Offset.Zero
+				var lastRadian = 0f
 
 				detectDragGestures(
 					onDragStart = {
@@ -44,8 +48,27 @@ fun ScientificDatePicker(
 
 						val target = accumulated + startEarthOffset
 
-						val radian = atan2(target.y - size.height / 2f, target.x - size.width / 2f)
+						val radian = atan2(target.y - size.height / 2f, target.x - size.width / 2f).let {
+							if (it < 0) it + PI_F*2
+							else it
+						}
 						earthOffset = getOffsetByAngle(size, radian)
+
+						val year =
+							if (lastRadian in PI_F * 0.5f..PI_F && radian in PI_F..PI_F * 1.5f) {
+								date.year + 1
+							} else if (lastRadian in PI_F..PI_F * 1.5f && radian in PI_F * 0.5f..PI_F) {
+								date.year - 1
+							} else {
+								date.year
+							}
+
+						lastRadian = radian
+
+						val days = if (isLeapYear(year)) 366 else 365
+						val progress = (radian + PI_F) / (2*PI_F) - 0.5f
+						val nthDay = ceil(days * progress).toInt().coerceIn(1, days)
+						onDateChange(LocalDate.ofYearDay(year, nthDay))
 					}
 				)
 			}
@@ -70,4 +93,8 @@ private fun getOffsetByAngle(size: IntSize, radian: Float): Offset {
 		x = horizontalRadius * cos(radian) + horizontalRadius,
 		y = verticalRadius * sin(radian) + verticalRadius
 	)
+}
+
+private fun isLeapYear(year: Int): Boolean {
+	return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 }
